1ead34e4f29941c8f530b6906faea55c26d06eae
Lakshman Doke <lakshman.doke@godigit.com>
2022-04-04 13:10:47 +0530

    commit

--- src/main/java/com/laksh/controller/PhoneBookController.java
+++ src/main/java/com/laksh/controller/PhoneBookController.java
@@ -13,17 +13,30 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 public class PhoneBookController {
-	@Autowired
-	private PhoneBookRepo phoneBookRepo;
-	
-	@PostMapping("/save")
-	public PhoneNoEntity storePhoneNumber(@RequestBody PhoneNo phoneNo) {
-		PhoneNoEntity noEntity = new PhoneNoEntity();
-		BeanUtils.copyProperties(phoneNo, noEntity);
-		 return  phoneBookRepo.save(noEntity);
-	}
-	@GetMapping("/get-contact")
-	public List<PhoneNoEntity> getAllContact(){
-		return phoneBookRepo.findAll();
-	}
+    @Autowired
+    private PhoneBookRepo phoneBookRepo;
+
+    @PostMapping("/save")
+    public PhoneNoEntity storePhoneNumber(@RequestBody PhoneNo phoneNo) {
+        PhoneNoEntity noEntity = new PhoneNoEntity();
+        BeanUtils.copyProperties(phoneNo, noEntity);
+        return phoneBookRepo.save(noEntity);
+    }
+
+    @GetMapping("/get-contact")
+    public List<PhoneNoEntity> getAllContact() {
+        return phoneBookRepo.findAll();
+    }
+
+    @DeleteMapping("/delete")
+    public Boolean deletePhoneNumber(@RequestParam Long phoneNumber) {
+        try {
+            List<PhoneNoEntity> phoneNoEntities = phoneBookRepo.findByPhoneNumber(phoneNumber);
+            phoneNoEntities.forEach(x -> phoneBookRepo.delete(x));
+            return true;
+        } catch (Exception ignored) {
+
+        }
+        return false;
+    }
 }
--- src/main/java/com/laksh/repositories/PhoneBookRepo.java
+++ src/main/java/com/laksh/repositories/PhoneBookRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.laksh.entities.PhoneNoEntity;
 
+import java.util.List;
+
 public interface PhoneBookRepo extends JpaRepository<PhoneNoEntity, Long>{
 
+    List<PhoneNoEntity> findByPhoneNumber(Long phoneNumber);
 }
